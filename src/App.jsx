5d31b5bc5c6feb3996dcf4529b7993130c68cd5b
dassi <d0583209585@gmail.com>
2026-04-28 23:23:05 +0300

    the second lesson

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,32 @@ import './App.css'
 import Post from './components/post'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
+
+  const [posts, setPosts] = useState([
+    {name: "dassi", content: "my post content"},
+    {name: "dassi2", content: "my post content2"},
+    {name: "dassi3", content: "my post content3"}
+  ]);
+
+  const handleEditPost = (newContent, index) => {
+    setPosts(prevPosts => {
+      const updatedPosts = [...prevPosts];
+      updatedPosts[index].content = newContent;
+      return updatedPosts;
+    });
+  }
+  
+  const handleLogPosts = () => {
+    console.log(posts);
+  }
 
   return (
     <>
-    <Post name="dassi" content="my post content" />
-    <Post name="dassi2" content="my post content2" />
-    <Post name="dassi3" content="my post content3" />
+    <div>
+      {posts.map((post, index) => (<Post key={index} name={post.name} content={post.content} onEdit={(newContent) => handleEditPost(newContent, index)} />))}
+    </div>
+    <button onClick={handleLogPosts}>log the posts</button>
     </>
   )
 }
--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,17 +1,44 @@
 import { useState } from "react";
-function Post({name, content}) {
+
+function Post({name, content, onEdit}) {
 
     const [post_content, setPost_content] = useState(content);
+    const [isEditing, setIsEditing] = useState(false);
 
     const handleInputChange = (event) => {
         setPost_content(event.target.value);
+    }
+    //const [changed_content, setChanged_content] = useState(content);
+
+
+    const handleEdit = () => {
+        setIsEditing(true);
+        //setChanged_content(post_content);
+    }
+
+    const handleSaveChanges = () => {
+        onEdit(post_content);
+        setIsEditing(false);
+    }
+
+    const handleCancelChanges = () => {
+        setPost_content(content);
+        setIsEditing(false);
     }
 
     return (
         <div style={{border: "1px solid white", margin: "10px", padding: "10px"}}>
             <h1>{name}</h1>
-            <p>{post_content}</p>
-            <textarea id="changed_content" placeholder="write something..." onChange={handleInputChange} />
+            <p>{content}</p>
+            {isEditing ? (
+                <>
+                    <textarea id="changed_content" value={post_content} onChange={handleInputChange} /><br />
+                    <button onClick={handleSaveChanges}>save</button>
+                    <button onClick={handleCancelChanges}>cancel</button>
+                </>
+            ) : (
+                <button onClick={handleEdit}>edit</button>
+            )}
         </div>
     )
 }
